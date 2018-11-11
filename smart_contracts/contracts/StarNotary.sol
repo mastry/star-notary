1ec5eff9dd9e282a8413511645b4811472a37c07
Jeff Mastry <jeff@mastry.net>
2018-11-10 22:05:35 -0500

    Add Star properties

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -1,18 +1,22 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract StarNotary is ERC721 { 
 
     struct Star { 
         string name; 
+        string rightAscension;
+        string declination;
+        string magnitude;
+        string story;
     }
 
     mapping(uint256 => Star) public tokenIdToStarInfo; 
     mapping(uint256 => uint256) public starsForSale;
 
-    function createStar(string _name, uint256 _tokenId) public { 
-        Star memory newStar = Star(_name);
+    function createStar(string _name, string _ra, string _dec, string _mag, string _story, uint256 _tokenId) public { 
+        Star memory newStar = Star(_name, _ra, _dec, _mag, _story);
 
         tokenIdToStarInfo[_tokenId] = newStar;
 
@@ -20,17 +24,17 @@ contract StarNotary is ERC721 {
     }
 
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
-        require(this.ownerOf(_tokenId) == msg.sender);
+        require(this.ownerOf(_tokenId) == msg.sender, "Caller is not the star owner.");
 
         starsForSale[_tokenId] = _price;
     }
 
     function buyStar(uint256 _tokenId) public payable { 
-        require(starsForSale[_tokenId] > 0);
+        require(starsForSale[_tokenId] > 0, "Star is not for sale.");
         
         uint256 starCost = starsForSale[_tokenId];
         address starOwner = this.ownerOf(_tokenId);
-        require(msg.value >= starCost);
+        require(msg.value >= starCost, "Payment amount is not sufficient.");
 
         _removeTokenFrom(starOwner, _tokenId);
         _addTokenTo(msg.sender, _tokenId);
